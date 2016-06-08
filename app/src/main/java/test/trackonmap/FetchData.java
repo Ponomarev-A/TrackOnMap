ddb09950b184da67fa1cfb779d8c1cf00228b25e
Nik Dyonin <nik@brite-apps.com>
2016-06-09 00:00:20 +0300

    Правки для иллюстрации комментариев.

--- app/src/main/java/test/trackonmap/FetchData.java
+++ app/src/main/java/test/trackonmap/FetchData.java
@@ -1,6 +1,7 @@
 package test.trackonmap;
 
 import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,70 +12,65 @@ import java.util.ArrayList;
  * Fetch data from JSON file class
  */
 public class FetchData {
+	private static final String LOG_TAG = FetchData.class.getSimpleName();
 
-    private static final String LOG_TAG = FetchData.class.getSimpleName();
+	public static Track getTrackFromJson(String jsonStr) {
+		// General tracks information
+		final String OWM_STATE = "state";
+		final String OWM_MESSAGE = "msg";
 
-    public static Track getTrackFromJson(String jsonStr) {
+		// General track information.  Each track info is an element of the "data" array.
+		final String OWM_DATA = "data";
 
-        // General tracks information
-        final String OWM_STATE = "state";
-        final String OWM_MESSAGE = "msg";
+		final String OWM_START = "start";
+		final String OWM_END = "end";
+		final String OWM_LENGTH = "length";
+		final String OWM_COUNT = "count";
+		final String OWM_MAX_SPEED = "max_speed";
 
-        // General track information.  Each track info is an element of the "data" array.
-        final String OWM_DATA = "data";
+		// Track points information.  Each track point info is an element of the "points" array.
+		final String OWM_POINTS = "points";
 
-        final String OWM_START = "start";
-        final String OWM_END = "end";
-        final String OWM_LENGTH = "length";
-        final String OWM_COUNT = "count";
-        final String OWM_MAX_SPEED = "max_speed";
+		final String OWM_LATITUDE = "lat";
+		final String OWM_LONGITUDE = "lng";
+		final String OWM_DATE = "date";
+		final String OWM_SPEED = "speed";
 
-        // Track points information.  Each track point info is an element of the "points" array.
-        final String OWM_POINTS = "points";
+		try {
+			JSONObject trackArrayJson = new JSONObject(jsonStr);
+			JSONObject trackJson = trackArrayJson.getJSONArray(OWM_DATA).getJSONObject(0);
 
-        final String OWM_LATITUDE = "lat";
-        final String OWM_LONGITUDE = "lng";
-        final String OWM_DATE = "date";
-        final String OWM_SPEED = "speed";
+			long startTime = trackJson.getLong(OWM_START) * 1000;   // convert to Ms
+			long endTime = trackJson.getLong(OWM_END) * 1000;
+			int length = trackJson.getInt(OWM_LENGTH);
+			int count = trackJson.getInt(OWM_COUNT);
+			int max_speed = trackJson.getInt(OWM_MAX_SPEED);
 
-        try {
-            JSONObject trackArrayJson = new JSONObject(jsonStr);
-            JSONObject trackJson = trackArrayJson.getJSONArray(OWM_DATA).getJSONObject(0);
+			JSONArray pointsArray = trackJson.getJSONArray(OWM_POINTS);
 
+			ArrayList<TrackPoint> points = new ArrayList<>();
 
-                long startTime = trackJson.getLong(OWM_START) * 1000;   // convert to Ms
-                long endTime = trackJson.getLong(OWM_END) * 1000;
-                int length = trackJson.getInt(OWM_LENGTH);
-                int count = trackJson.getInt(OWM_COUNT);
-                int max_speed = trackJson.getInt(OWM_MAX_SPEED);
+			for (int j = 0; j < pointsArray.length(); j++) {
+				JSONObject pointJson = pointsArray.getJSONObject(j);
 
-                JSONArray pointsArray = trackJson.getJSONArray(OWM_POINTS);
+				double latitude = pointJson.getDouble(OWM_LATITUDE);
+				double longitude = pointJson.getDouble(OWM_LONGITUDE);
+				long date = pointJson.getLong(OWM_DATE);
+				int speed = pointJson.getInt(OWM_SPEED);
 
-                ArrayList<TrackPoint> points = new ArrayList<>();
+				points.add(new TrackPoint(latitude, longitude, date, speed));
+			}
 
-                for (int j = 0; j < pointsArray.length(); j++) {
-                    JSONObject pointJson = pointsArray.getJSONObject(j);
+			Track track = new Track(startTime, endTime, length, count, max_speed, points);
+			Log.d(LOG_TAG, "Fetch data form JSON complete. " + track.getCount() + " track points inserted");
 
-                    double latitude = pointJson.getDouble(OWM_LATITUDE);
-                    double longitude = pointJson.getDouble(OWM_LONGITUDE);
-                    long date = pointJson.getLong(OWM_DATE);
-                    int speed = pointJson.getInt(OWM_SPEED);
+			return track;
+		}
+		catch (JSONException e) {
+			Log.e(LOG_TAG, e.getMessage(), e);
+			e.printStackTrace();
+		}
 
-                    points.add(new TrackPoint(latitude, longitude, date, speed));
-                }
-
-            Track track = new Track(startTime, endTime, length, count, max_speed, points);
-            Log.d(LOG_TAG, "Fetch data form JSON complete. " + track.getCount() + " track points inserted");
-
-            return track;
-
-        } catch (JSONException e) {
-            Log.e(LOG_TAG, e.getMessage(), e);
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-
+		return null;
+	}
 }
--- app/src/main/java/test/trackonmap/TrackPoint.java
+++ app/src/main/java/test/trackonmap/TrackPoint.java
@@ -6,26 +6,25 @@ import com.google.android.gms.maps.model.LatLng;
  * Track point information class
  */
 public class TrackPoint {
+	private final long date;
+	private final int speed;
+	private final LatLng latLng;
 
-    private final long date;
-    private final int speed;
-    private final LatLng latLng;
+	public TrackPoint(double latitude, double longitude, long date, int speed) {
+		latLng = new LatLng(latitude, longitude);
+		this.date = date;
+		this.speed = speed;
+	}
 
-    public TrackPoint(double latitude, double longitude, long date, int speed) {
-        latLng = new LatLng(latitude, longitude);
-        this.date = date;
-        this.speed = speed;
-    }
+	public long getDate() {
+		return date;
+	}
 
-    public long getDate() {
-        return date;
-    }
+	public int getSpeed() {
+		return speed;
+	}
 
-    public int getSpeed() {
-        return speed;
-    }
-
-    public LatLng getLatLng() {
-        return latLng;
-    }
+	public LatLng getLatLng() {
+		return latLng;
+	}
 }
