2509f5648fe7ca0d0c56a7fa4fc38b06d3e8ac30
Nik Dyonin <wolf.step@gmail.com>
2016-06-09 20:17:34 +0300

    Правки для иллюстрации комментариев. (#1)

--- app/src/main/java/test/trackonmap/MainActivity.java
+++ app/src/main/java/test/trackonmap/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -26,170 +27,175 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+	private static final int COLOR_GREEN_SPEED_KM_H = 60;
+	private static final int COLOR_YELLOW_SPEED_KM_H = 120;
+	private static final String KEY_TRACK = "INFO_TRACK";
+	private static final String TAG_INFO_FRAGMENT = "infoTrack";
+	private GoogleMap mMap;
+	private Track mTrack = null;
 
-    private static final int COLOR_GREEN_SPEED_KM_H = 60;
-    private static final int COLOR_YELLOW_SPEED_KM_H = 120;
-    private static final String KEY_TRACK = "INFO_TRACK";
-    private static final String TAG_INFO_FRAGMENT = "infoTrack";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    private GoogleMap map;
-    private Track track = null;
-    private DownloadJSON downloader;
+		SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	public void onMapReady(GoogleMap googleMap) {
+		mMap = googleMap;
+		mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+			@Override
+			public void onCameraChange(CameraPosition cameraPosition) {
+				if (mTrack != null) {
+					drawTrack();
+				}
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+				// Remove listener to prevent position reset on camera move.
+				mMap.setOnCameraChangeListener(null);
+			}
+		});
 
-        // Start async downloader task
-        downloader = new DownloadJSON();
-        downloader.execute();
-    }
+		new DownloadJSON() {
+			@Override
+			protected void onPostExecute(String s) {
+				mTrack = FetchData.getTrackFromJson(s);
+				drawTrack();
+			}
+		}.execute();
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-            @Override
-            public void onCameraChange(CameraPosition cameraPosition) {
-                if (track != null) {
-                    drawTrackOnMap(track, map);
-                }
-                // Remove listener to prevent position reset on camera move.
-                map.setOnCameraChangeListener(null);
-            }
-        });
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		final int id = item.getItemId();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+		if (id == R.id.action_infoTrack) {
+			if (mTrack != null) {
+				InfoFragment infoFragment = new InfoFragment();
+				Bundle bundle = new Bundle();
+				bundle.putSerializable(KEY_TRACK, mTrack);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+				infoFragment.setArguments(bundle);
+				infoFragment.show(getFragmentManager(), TAG_INFO_FRAGMENT);
+			}
+			else {
+				// TODO: show Toast with "Track not loaded" label
+			}
+		}
 
-        int id = item.getItemId();
-        if (id == R.id.action_infoTrack) {
-            InfoFragment infoFragment = new InfoFragment();
-            Bundle bundle = new Bundle();
-            bundle.putSerializable(KEY_TRACK, track);
+		return super.onOptionsItemSelected(item);
+	}
 
-            infoFragment.setArguments(bundle);
-            infoFragment.show(getFragmentManager(), TAG_INFO_FRAGMENT);
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private void drawTrack() {
+		if (mMap == null || mTrack == null) {
+			return;
+		}
 
-    private void drawTrackOnMap(Track track, GoogleMap map) {
-        // Get point and draw line with coordinates (prevPoint(lat, lng), nextPoint(lat, lng)))
-        List<TrackPoint> points = track.getPoints();
-        TrackPoint startPoint = points.get(0);
-        TrackPoint endPoint = points.get(points.size() - 1);
+		// Get point and draw line with coordinates (prevPoint(lat, lng), nextPoint(lat, lng)))
+		List<TrackPoint> points = mTrack.getPoints();
+		TrackPoint startPoint = points.get(0);
+		TrackPoint endPoint = points.get(points.size() - 1);
 
-        TrackPoint prevPoint = startPoint;
+		TrackPoint prevPoint = startPoint;
 
-        for (TrackPoint point : points) {
-            map.addPolyline(new PolylineOptions()
-                    .add(prevPoint.getLatLng())
-                    .add(point.getLatLng())
-                    .color(getLineColor(prevPoint, point))
-            );
-            prevPoint = point;
-        }
+		for (TrackPoint point : points) {
+			mMap.addPolyline(new PolylineOptions().add(prevPoint.getLatLng()).add(point.getLatLng()).color(getLineColor(prevPoint, point)));
+			prevPoint = point;
+		}
 
-        LatLngBounds.Builder builder = LatLngBounds.builder()
-                .include(startPoint.getLatLng())
-                .include(endPoint.getLatLng());
+		LatLngBounds.Builder builder = LatLngBounds.builder().include(startPoint.getLatLng()).include(endPoint.getLatLng());
 
-        map.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 30));
-    }
+		mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(builder.build(), 30));
+	}
 
-    private int getLineColor(TrackPoint prevPoint, TrackPoint nextPoint) {
-        int speed = Math.max(prevPoint.getSpeed(), nextPoint.getSpeed());
+	private int getLineColor(TrackPoint prevPoint, TrackPoint nextPoint) {
+		final int speed = Math.max(prevPoint.getSpeed(), nextPoint.getSpeed());
 
-        if (speed <= COLOR_GREEN_SPEED_KM_H) {
-            return Color.GREEN;
-        } else if (speed <= COLOR_YELLOW_SPEED_KM_H) {
-            return Color.YELLOW;
-        } else {
-            return Color.RED;
-        }
-    }
+		if (speed <= COLOR_GREEN_SPEED_KM_H) {
+			return Color.GREEN;
+		}
+		else if (speed <= COLOR_YELLOW_SPEED_KM_H) {
+			return Color.YELLOW;
+		}
+		else {
+			return Color.RED;
+		}
+	}
 
-    public static class InfoFragment extends DialogFragment {
-        private Track track;
+	public static class InfoFragment extends DialogFragment {
+		private Track mTrack;
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-            Bundle arguments = getArguments();
-            if (arguments != null) {
-                track = (Track) arguments.getSerializable(KEY_TRACK);
-            }
+			Bundle arguments = getArguments();
 
-            builder.setTitle(getString(R.string.dialog_title))
-                    .setMessage(getInfoTrack())
-                    .setPositiveButton(getString(R.string.dialog_button_title), new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.cancel();
-                        }
-                    });
-            return builder.create();
-        }
+			if (arguments != null) {
+				mTrack = (Track)arguments.getSerializable(KEY_TRACK);
+			}
 
-        private String getInfoTrack() {
-            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy", Locale.ENGLISH);
+			builder.setTitle(getString(R.string.dialog_title)).setMessage(getInfoTrack()).setPositiveButton(getString(R.string.dialog_button_title), new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+				}
+			});
 
-            return String.format(getString(R.string.format_info_track),
-                    track.getLength() / 1000f,  // convert to kilometers
-                    track.getMaxSpeed(),
-                    dateFormat.format(new Date(track.getStartTime())),
-                    timeFormat.format(new Date(track.getStartTime())),
-                    timeFormat.format(new Date(track.getEndTime()))
-            );
-        }
-    }
+			return builder.create();
+		}
 
-    private class DownloadJSON extends AsyncTask<Void, Void, String> {
+		private String getInfoTrack() {
+			SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.US);
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy", Locale.US);
 
-        @Override
-        protected String doInBackground(Void... params) {
-            // Mock data load from local file
-            return loadFromFile();
-        }
+			return String.format(getString(R.string.format_info_track), mTrack.getLength() / 1000f,  // convert to kilometers
+					mTrack.getMaxSpeed(), dateFormat.format(new Date(mTrack.getStartTime())), timeFormat.format(new Date(mTrack.getStartTime())), timeFormat.format(new Date(mTrack.getEndTime())));
+		}
+	}
 
-        @Override
-        protected void onPostExecute(String s) {
-            track = FetchData.getTrackFromJson(s);
-        }
+	private class DownloadJSON extends AsyncTask<Void, Void, String> {
+		@Override
+		protected String doInBackground(Void... params) {
+			// Mock data load from local file
+			return loadFromFile();
+		}
 
-        private String loadFromFile() {
+		private String loadFromFile() {
+			String json = null;
+			InputStream is = null;
 
-            String json = null;
+			try {
+				is = getAssets().open("track.json");
 
-            try {
-                InputStream is = getAssets().open("track.json");
+				byte[] buffer = new byte[is.available()];
+				is.read(buffer);
 
-                byte[] buffer = new byte[is.available()];
-                is.read(buffer);
-                is.close();
+				json = new String(buffer, "UTF-8");
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				if (is != null) {
+					try {
+						is.close();
+					}
+					catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 
-                json = new String(buffer, "UTF-8");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return json;
-        }
-    }
+			return json;
+		}
+	}
 }
 
--- app/src/main/java/test/trackonmap/Track.java
+++ app/src/main/java/test/trackonmap/Track.java
@@ -7,44 +7,44 @@ import java.util.List;
  * Track information class
  */
 public class Track implements Serializable {
-    private final long startTime;
-    private final long endTime;
-    private final int length;
-    private final int max_speed;
+	private final long startTime;
+	private final long endTime;
+	private final int length;
+	private final int max_speed;
 
-    private int count;
-    private List<TrackPoint> points;
+	private int count;
+	private List<TrackPoint> points;
 
-    public Track(long startTime, long endTime, int length, int count, int max_speed, List<TrackPoint> points) {
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.length = length;
-        this.count = count;
-        this.max_speed = max_speed;
-        this.points = points;
-    }
+	public Track(long startTime, long endTime, int length, int count, int max_speed, List<TrackPoint> points) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.length = length;
+		this.count = count;
+		this.max_speed = max_speed;
+		this.points = points;
+	}
 
-    public long getStartTime() {
-        return startTime;
-    }
+	public long getStartTime() {
+		return startTime;
+	}
 
-    public long getEndTime() {
-        return endTime;
-    }
+	public long getEndTime() {
+		return endTime;
+	}
 
-    public int getLength() {
-        return length;
-    }
+	public int getLength() {
+		return length;
+	}
 
-    public int getMaxSpeed() {
-        return max_speed;
-    }
+	public int getMaxSpeed() {
+		return max_speed;
+	}
 
-    public int getCount() {
-        return count;
-    }
+	public int getCount() {
+		return count;
+	}
 
-    public List<TrackPoint> getPoints() {
-        return points;
-    }
+	public List<TrackPoint> getPoints() {
+		return points;
+	}
 }
